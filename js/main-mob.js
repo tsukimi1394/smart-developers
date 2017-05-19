2528a593e6b6c1b0ccd5658f939dbada2590b30e
Daria Ganchukova <umi194@gmail.com>
2017-05-19 14:52:09 +0300

    added slider to mobile

--- js/main-mob.js
+++ js/main-mob.js
@@ -55,3 +55,36 @@ function PopUpShow(){
 function PopUpHide(){
     $(".location-popup").hide();
 }
+
+var wallopEl = document.querySelector('.Wallop');
+var wallop = new Wallop(wallopEl);
+
+var paginationDots = Array.prototype.slice.call(document.querySelectorAll('.Wallop-dot'));
+
+/*
+Attach click listener on the dots
+*/
+paginationDots.forEach(function (dotEl, index) {
+  dotEl.addEventListener('click', function() {
+    wallop.goTo(index);
+  });
+});
+
+/*
+Listen to wallop change and update classes
+*/
+wallop.on('change', function(event) {
+  removeClass(document.querySelector('.Wallop-dot--current'), 'Wallop-dot--current');
+  addClass(paginationDots[event.detail.currentItemIndex], 'Wallop-dot--current');
+});
+
+// Helpers
+function addClass(element, className) {
+  if (!element) { return; }
+  element.className = element.className.replace(/\s+$/gi, '') + ' ' + className;
+}
+
+function removeClass(element, className) {
+  if (!element) { return; }
+  element.className = element.className.replace(className, '');
+}
